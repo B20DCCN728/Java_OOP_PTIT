f4ee010b05c9faaa281dd41004cb9d7857873a72
Campus <108795111+Campus-is-me@users.noreply.github.com>
2022-08-20 09:30:58 +0700

    Update Tinh_Luy_Thua_J01021.java

--- Tinh_Luy_Thua_J01021.java
+++ Tinh_Luy_Thua_J01021.java
@@ -4,13 +4,16 @@ public class Tinh_Luy_Thua {
 
     static int mod = (int)1e9 + 7;
 
-    static long Pow(long a, long b)
-    {
+    static long Pow_2(long a, long b){
+        if(b == 1) return a;
+        long  k = Pow_2(a, b / 2);
+        if(b % 2 == 1) return (((k * k) % mod) * a) % mod;
+        else return (k * k) % mod; // <--- MLE =))
+    }
+    static long Pow(long a, long b){
         long res = 1;
-        while(b > 0)
-        {
-            if(b % 2 == 1)
-                res = res * a % mod;
+        while(b > 0){
+            if(b % 2 == 1) res = (res * a) % mod;
             a = (a * a) % mod;
             b >>= 1;
         }
