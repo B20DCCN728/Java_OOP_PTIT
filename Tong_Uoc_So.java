1510029fc9bb051368dd3d9d6a50fe3143813de1
Campus <108795111+Campus-is-me@users.noreply.github.com>
2022-08-20 09:27:52 +0700

    Add files via upload

---
+++ Tong_Uoc_So.java
@@ -0,0 +1,32 @@
+import java.util.*;
+public class Tong_Uoc_So {
+    static int mod = (int)1e6 * 2;
+    static int[] era = new int[mod + 5];
+    static void Era(){
+        for(int i = 2;i <= Math.sqrt(mod);i++){
+            if(era[i] == 0){
+                for(int j = i * i; j <= mod;j += i){
+                    if(era[j] == 0) era[j] = i;
+                }
+            }
+        }
+        for(int i = 2;i <= mod;i++){
+            if(era[i] == 0) era[i] = i;
+        }
+    }
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        Era();
+        long res = 0;
+        while(t-- > 0){
+            int n = sc.nextInt();
+            while(n != 1){
+                res += era[n];
+                n /= era[n];
+            }
+        }
+        System.out.println(res);
+    }
+}
