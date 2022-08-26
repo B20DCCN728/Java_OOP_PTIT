d2133b2be6138cdf7d9c1ff3254dea9713258303
Campus <108795111+Campus-is-me@users.noreply.github.com>
2022-08-27 00:15:51 +0700

    Add files via upload

---
+++ Tong_So_Nguyen_Lon_2.java
@@ -0,0 +1,18 @@
+package tong_so_nguyen_lon_2;
+import java.util.*;
+import java.lang.*;
+import java.math.*;
+public class Tong_So_Nguyen_Lon_2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner sc = new Scanner(System.in);
+        BigInteger num_1 = sc.nextBigInteger();
+        BigInteger num_2 = sc.nextBigInteger();
+        System.out.println(num_1.add(num_2));
+    }
+    
+}
