67375719cc027e6ba779c35bb4471013646065b9
Campus <108795111+Campus-is-me@users.noreply.github.com>
2022-09-14 22:53:39 +0700

    Update J05020_Sap_Xep_Sinh_Vien_Theo_Lop.java

--- J05020_Sap_Xep_Sinh_Vien_Theo_Lop.java
+++ J05020_Sap_Xep_Sinh_Vien_Theo_Lop.java
@@ -1,4 +1,4 @@
-package sap_xep_sinh_vien_theo_lop;
+package liet_ke_sinh_vien_theo_lop;
 import java.io.*;
 import java.lang.*;
 import java.math.*;
@@ -17,23 +17,13 @@ class Student{
         return this._class;
     }
     
-    protected String _getCode(){
-        return this._code;
-    }
-    
     @Override
     public String toString(){
         return this._code + " " + this._name + " " + this._class + " " + this._email;
     }
 } 
-class sortTheArrayList implements Comparator<Student>{
-    @Override
-    public int compare(Student o1, Student o2){
-        if(o1._getClass().compareTo(o2._getClass()) != 0) return o1._getClass().compareTo(o2._getClass());
-        return o1._getCode().compareTo(o2._getCode());
-    }
-}
-public class Sap_Xep_Sinh_Vien_Theo_Lop {
+public class Liet_Ke_Sinh_Vien_Theo_Lop{
+    
     public static void main(String[] args) {
         // TODO code application logic here
         Scanner sc = new Scanner(System.in);
@@ -41,10 +31,18 @@ public class Sap_Xep_Sinh_Vien_Theo_Lop {
         ArrayList<Student> solve = new ArrayList<>();
         while(t-- > 0){
             solve.add(new Student(sc.nextLine(), sc.nextLine(), sc.nextLine(), sc.nextLine()));
-        }
-        Collections.sort(solve, new sortTheArrayList());
-        for(Student x : solve){
-            System.out.println(x);
+        }      
+        int numQuery = Integer.parseInt(sc.nextLine());
+        while(numQuery-- > 0){
+            String compare = sc.nextLine();
+            System.out.println("DANH SACH SINH VIEN LOP " + compare + ":");
+            for(Student x : solve){
+                if(x._getClass().compareTo(compare) == 0){
+                    System.out.println(x);
+                }
+            }
         }
     }
+    
 }
+
