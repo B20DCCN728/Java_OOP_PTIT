670e74e4a1d357b89ea8f7d3fa61a638f6385482
Campus <108795111+Campus-is-me@users.noreply.github.com>
2022-10-04 22:34:38 +0700

    Add files via upload

---
+++ Danh_Sach_Thuc_Tap_2.java
@@ -0,0 +1,28 @@
+//Created by Campus
+package danh_sach_thuc_tap_2;
+import java.lang.*;
+import java.math.*;
+import java.io.*;
+import java.text.*;
+import java.util.*;
+//
+public class Danh_Sach_Thuc_Tap_2 {
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner sc = new Scanner(System.in);
+        int t = Integer.parseInt(sc.nextLine());
+        ArrayList<Trainning> solve = new ArrayList<>();
+        while(t-- > 0){
+            solve.add(new Trainning(sc.nextLine(), sc.nextLine(), sc.nextLine(), sc.nextLine(), sc.nextLine()));
+        }
+        Collections.sort(solve);
+        int n = Integer.parseInt(sc.nextLine());
+        while(n-- > 0){
+            String query = sc.nextLine();
+            for(Trainning x: solve){
+                if(x.getEnterprise().compareTo(query) == 0) System.out.println(x);
+            }
+        }
+    }
+    
+}
