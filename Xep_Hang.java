c0aef24fb1f6cd29e17684ab2c9169419fc94b0e
Campus <108795111+Campus-is-me@users.noreply.github.com>
2022-10-14 22:24:54 +0700

    Add files via upload

---
+++ Xep_Hang.java
@@ -0,0 +1,33 @@
+//Created by Campus
+package xep_hang;
+import java.util.*;
+import java.lang.*;
+import java.text.*;
+import java.io.*;
+//
+public class Xep_Hang {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        TreeMap<Integer, Integer> solve = new TreeMap<>();
+        while(t-- > 0){
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+            if(solve.containsKey(x)){
+                y += solve.get(x);
+            }
+            solve.put(x, y);
+        }
+        int time = 0;
+        for(Map.Entry<Integer, Integer> it: solve.entrySet()){
+            if(time < it.getKey()) time = it.getKey();
+            time += it.getValue();
+        }
+        System.out.println(time);
+    }
+    
+}
