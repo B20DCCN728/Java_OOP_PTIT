01b6c5548feaaa3fc5cfa4ce0ab98e3e9ce43e0f
Campus <108795111+Campus-is-me@users.noreply.github.com>
2022-10-23 09:58:41 +0700

    Update and rename Dang_Ki_Hinh_Thuc_Giang_Day.java to J07073_Dang_Ki_Hinh_Thuc_Giang_Day.java

--- J07073_Dang_Ki_Hinh_Thuc_Giang_Day.java
+++ J07073_Dang_Ki_Hinh_Thuc_Giang_Day.java
@@ -6,6 +6,27 @@ import java.lang.*;
 import java.text.*;
 import java.math.*;
 //
+class Problem implements Comparable<Problem> {
+    private String code, name, stc, ll;
+    public String th;
+    public Problem(String code, String name, String stc, String ll, String th){
+        this.code = code;
+        this.name = name;
+        this.stc = stc;
+        this.ll = ll;
+        this.th = th;
+    }
+    @Override
+    public int compareTo(Problem x){
+        return this.code.compareTo(x.code);
+    }
+    @Override 
+    public String toString(){
+        return this.code + " " + this.name + " " + this.stc + " " + this.ll + " " + this.th;
+    }
+}
+
+//
 public class Dang_Ki_Hinh_Thuc_Giang_Day {
     public static void main(String[] args) throws FileNotFoundException {
         // TODO code application logic here
