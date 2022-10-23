1f7b8c47ba55f088bc514dc8ceff82ded47cac42
Campus <108795111+Campus-is-me@users.noreply.github.com>
2022-10-23 18:08:59 +0700

    Add files via upload

---
+++ Chia_Het_Cho_11.java
@@ -0,0 +1,26 @@
+package chia_het_cho_11;
+//
+import java.util.*;
+import java.lang.*;
+import java.math.*;
+//
+public class Chia_Het_Cho_11 {
+    static long Mod(String solve){
+        long mod = 0, tmp;
+        for(int i = 0;i < solve.length();i++){
+            mod = (mod * 10 + solve.charAt(i) - '0') % 11; 
+        }
+        return mod;
+    }
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner sc = new Scanner(System.in);
+        int t = Integer.parseInt(sc.nextLine());
+        while(t-- > 0){
+            String solve = sc.nextLine();
+            if(Mod(solve) == 0) System.out.println("1");
+            else System.out.println("0");
+        }
+    }
+    
+}
