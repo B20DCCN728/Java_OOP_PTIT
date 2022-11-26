09fd22ad0ae9c67edb1950981b1759b5874db33e
Campus <108795111+Campus-is-me@users.noreply.github.com>
2022-11-26 16:45:49 +0700

    Update and rename Cau_Lac_Bo_Bong_Da.java to J05069_Cau_Lac_Bo_Bong_Da.java

--- J05069_Cau_Lac_Bo_Bong_Da.java
+++ J05069_Cau_Lac_Bo_Bong_Da.java
@@ -6,6 +6,42 @@ import java.math.*;
 import java.io.*;
 import java.text.*;
 //
+class InfoOfMatch {
+    private String code;
+    private int count, total;
+    public InfoOfMatch(String code, int count){
+        this.code = code;
+        this.count = count;
+        this.total = this.count * Club.Item.get(this.code.substring(1, 3)).getPrice();
+    }
+    @Override
+    public String toString(){
+        return this.code + " " + Club.Item.get(code.substring(1, 3)).getName() + " " + this.total;
+    }
+}
+//
+class Club {
+    private String code, name;
+    private int price;
+    static HashMap<String, Club> Item = new HashMap<>();
+    public Club(String code, String name, int price){
+        this.code = code;
+        this.name = name;
+        this.price = price;
+    }
+    public Club(){      
+    }
+    protected String getName(){
+        return this.name;
+    }
+    protected String getCode(){
+        return this.code;
+    }
+    protected int getPrice(){
+        return this.price;
+    }
+}
+//
 public class Cau_Lac_Bo_Bong_Da {
     /**
      * @param args the command line arguments
