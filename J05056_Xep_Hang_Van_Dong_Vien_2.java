973dd8c1acfe45734165963d293f6f597714e38d
Campus <108795111+Campus-is-me@users.noreply.github.com>
2022-11-27 00:10:45 +0700

    Update J05056_Xep_Hang_Van_Dong_Vien_2.java

--- J05056_Xep_Hang_Van_Dong_Vien_2.java
+++ J05056_Xep_Hang_Van_Dong_Vien_2.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+//Created by Campus
 package xep_hang_van_dong_vien_2;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
