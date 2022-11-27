84e10290bdc1ddbe180d76d47c63727de73f82fc
Campus <108795111+Campus-is-me@users.noreply.github.com>
2022-11-27 16:31:44 +0700

    Add files via upload

---
+++ PhongBan.java
@@ -0,0 +1,14 @@
+package tinh_luong;
+public class PhongBan {
+    private String code, name;
+    public PhongBan(String code, String name){
+        this.code = code;
+        this.name = name;
+    }
+    public String getCode(){
+        return this.code;
+    }
+    public String getName(){
+        return this.name;
+    }
+}
