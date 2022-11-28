ff5cf29a8e056690751139e713b36eaadcd699e7
Campus <108795111+Campus-is-me@users.noreply.github.com>
2022-11-28 16:57:58 +0700

    Update and rename Danh_Sach_Thuc_Tap_3.java to J07038_Danh_Sach_Thuc_Tap_3.java

--- J07038_Danh_Sach_Thuc_Tap_3.java
+++ J07038_Danh_Sach_Thuc_Tap_3.java
@@ -6,6 +6,62 @@ import java.math.*;
 import java.io.*;
 import java.text.*;
 //
+class SinhVien implements Comparable<SinhVien>{
+    private String code, name = "", group, email;
+    public SinhVien(String code, String nameNotFormat, String group, String email){
+        this.code = code;
+        String tmp[] = nameNotFormat.trim().split("\\s+");
+        for(int i = 0;i < tmp.length;i++){
+            this.name += Character.toUpperCase(tmp[i].charAt(0)) + tmp[i].substring(1).toLowerCase() + " ";
+        }
+        this.name = this.name.trim();
+        this.group = group;
+        this.email = email;
+    }
+    public String getCode(){
+        return code;
+    }
+    @Override
+    public int compareTo(SinhVien s){
+        return this.code.compareTo(s.code);
+    }
+    @Override
+    public String toString(){
+        return code + " " + name + " " + group; 
+    }
+}
+//
+class DoanhNghiep {
+    private String code, name;
+    private int tuyenDung, soLuong = 0;
+    private ArrayList<SinhVien> sinhVien = new ArrayList<>();
+    public DoanhNghiep(String code, String name, int tuyenDung){
+        this.code = code;
+        this.name = name;
+        this.tuyenDung = tuyenDung;
+    }
+    public int getSoLuong(){
+        return this.soLuong;
+    }
+    public int getTuyenDung(){
+        return this.tuyenDung;
+    }
+    public void setSoLuong(int soLuong, SinhVien a){
+        this.soLuong = soLuong;
+        sinhVien.add(a);
+    }
+    public String getCode(){
+        return this.code;
+    }
+    public void danhSachTuyenDung(){
+        System.out.println("DANH SACH THUC TAP TAI " + name + ":");
+        for(int i = 0;i < sinhVien.size();i++){
+            System.out.println(sinhVien.get(i));
+        }
+    }
+}
+
+//
 public class Danh_Sach_Thuc_Tap_3 {
     /**
      * @param args the command line arguments
