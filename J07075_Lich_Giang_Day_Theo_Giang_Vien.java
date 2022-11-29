a063fd91934bf785a56291e55589171446ebf4db
Campus <108795111+Campus-is-me@users.noreply.github.com>
2022-11-29 15:24:00 +0700

    Update and rename Lich_Giang_Day_Theo_Giang_Vien.java to J07075_Lich_Giang_Day_Theo_Giang_Vien.java

--- J07075_Lich_Giang_Day_Theo_Giang_Vien.java
+++ J07075_Lich_Giang_Day_Theo_Giang_Vien.java
@@ -6,6 +6,52 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Scanner;
+//
+class MonHoc {
+    private String maMon, tenMon;
+    private int tinChi;
+    public MonHoc(String maMon, String tenMon, int tinChi){
+        this.maMon = maMon;
+        this.tenMon = tenMon;
+        this.tinChi = tinChi;
+    }
+    public String getMaMon(){
+        return this.maMon;
+    }
+    public String getTenMon(){
+        return this.tenMon;
+    }
+}
+//
+class NhomLopHoc implements Comparable<NhomLopHoc>{
+    static int id = 0;
+    private String maNhom = "HP", maMon, giangVien, phongHoc;
+    private MonHoc monHoc;
+    private int ngayGD, kipHoc;
+    public NhomLopHoc(MonHoc monHoc, int ngayGD, int kipHoc, String giangVien, String phongHoc){
+        id += 1;
+        this.maNhom += String.format("%03d", id);
+        this.monHoc = monHoc;
+        this.ngayGD = ngayGD;
+        this.kipHoc = kipHoc;
+        this.giangVien = giangVien;
+        this.phongHoc = phongHoc;
+    }
+    public String getGiangVien(){
+        return giangVien;
+    }
+    @Override
+    public int compareTo(NhomLopHoc s){
+        if(this.ngayGD != s.ngayGD) return this.ngayGD - s.ngayGD;
+        if(this.kipHoc != s.kipHoc) return this.kipHoc - s.kipHoc;
+        return this.giangVien.compareTo(s.giangVien);
+    }
+    @Override
+    public String toString(){
+        return maNhom + " " + monHoc.getTenMon() + " " + ngayGD + " " + kipHoc + " " + phongHoc;
+    }
+}
+//
 public class Lich_Giang_Day_Theo_Giang_Vien {
     public static void main(String[] args) throws FileNotFoundException {
         // TODO code application logic here
