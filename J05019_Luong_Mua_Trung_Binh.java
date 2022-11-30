b775aeb50ab03874c91093fa7e0ef29aab91c4c8
Campus <108795111+Campus-is-me@users.noreply.github.com>
2022-11-30 17:00:05 +0700

    Update and rename Luong_Mua_Trung_Binh.java to J05019_Luong_Mua_Trung_Binh.java

--- J05019_Luong_Mua_Trung_Binh.java
+++ J05019_Luong_Mua_Trung_Binh.java
@@ -7,6 +7,37 @@ import java.io.*;
 import java.text.*;
 import java.util.concurrent.TimeUnit;
 //
+class Campus {
+    static int id = 0;
+    private String tenTram, code = "T";
+    private SimpleDateFormat myDate = new SimpleDateFormat("hh:mm");
+    private Date start, end;
+    private long luongMua, time;
+    public Campus(String tenTram, Date start, Date end, long luongMua){
+        id += 1;
+        this.code += String.format("%02d", id);
+        this.tenTram = tenTram;
+        this.start = start;
+        this.end = end;
+        this.luongMua = luongMua;
+        long tmp = this.end.getTime() - this.start.getTime();
+        this.time = TimeUnit.MINUTES.convert(tmp, TimeUnit.MILLISECONDS);
+    }
+    public String getTenTram(){
+        return tenTram;
+    }
+    public void setLuongMua(long luongMua, long time){
+        this.luongMua += luongMua;
+        this.time += time;
+    }
+    @Override
+    public String toString(){
+        double asw = (double)((double)this.luongMua / this.time) * 60;
+        return code + " " + tenTram + " " + String.format("%.2f", asw); 
+    }
+}
+
+//
 public class Luong_Mua_Trung_Binh {
     public static void main(String[] args) throws ParseException {
         // TODO code application logic here
