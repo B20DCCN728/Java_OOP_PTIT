c4a3412f2a43efb666621945f40f0f28e1c422bb
Campus <108795111+Campus-is-me@users.noreply.github.com>
2022-11-30 15:11:01 +0700

    Update and rename Quan_Li_Bai_Tap_Nhom.java to J07027_Quan_Li_Bai_Tap_Nhom.java

--- J07027_Quan_Li_Bai_Tap_Nhom.java
+++ J07027_Quan_Li_Bai_Tap_Nhom.java
@@ -6,6 +6,32 @@ import java.math.*;
 import java.io.*;
 import java.text.*;
 //
+class SinhVien implements Comparable<SinhVien>{
+    private String code, name, phoneNumber, homeWork;
+    private int gr;
+    public SinhVien(String code, String name, String phoneNumber){
+        this.code = code;
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+    }
+    public void setHomeWork(int gr, String homeWork){
+        this.homeWork = homeWork;
+        this.gr = gr;
+    }
+    public String getCode(){
+        return code;
+    }
+    @Override
+    public int compareTo(SinhVien s){
+        return this.code.compareTo(s.code);     
+    }
+    @Override
+    public String toString(){
+        return code + " " + name + " " + phoneNumber + " " + gr + " " + homeWork;
+    }
+}
+
+//
 public class Quan_Li_Bai_Tap_Nhom {
     public static void main(String[] args) throws FileNotFoundException {
         // TODO code application logic here
