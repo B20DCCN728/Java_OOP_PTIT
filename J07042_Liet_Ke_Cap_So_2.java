cb30ccff96d023d0c7f1caf63d88343c93eb0202
Campus <108795111+Campus-is-me@users.noreply.github.com>
2022-12-02 00:09:00 +0700

    Update and rename Liet_Ke_Cap_So_2.java to J07042_Liet_Ke_Cap_So_2.java

--- J07042_Liet_Ke_Cap_So_2.java
+++ J07042_Liet_Ke_Cap_So_2.java
@@ -7,6 +7,27 @@ import java.math.*;
 import java.io.*;
 import java.text.*;
 //
+class Pair implements Serializable, Comparable<Pair> {
+    private int first, second;
+    public Pair(int first, int second){
+        this.first = first;
+        this.second = second;
+    }
+    public int getFirst(){
+        return first;
+    }
+    public int getSecond(){
+        return second;
+    }
+    @Override
+    public int compareTo(Pair o){
+        return this.first - o.first;
+    }
+    public String toString(){
+        return "(" + first + ", " + second + ")";
+    }
+}
+//
 //public class Liet_Ke_Cap_So_2 {
 public class Test {
     static boolean isCheck(int a, int b){
