e5dc726e7b950a52363b172b5fdb5a827fa3ccd9
Campus <108795111+Campus-is-me@users.noreply.github.com>
2022-12-01 22:18:12 +0700

    Update Liet_Ke_Cap_So_1.java

--- Liet_Ke_Cap_So_1.java
+++ Liet_Ke_Cap_So_1.java
@@ -6,6 +6,29 @@ import java.math.*;
 import java.io.*;
 import java.text.*;
 //
+class Pair implements Serializable, Comparable<Pair> {
+    private int first, second;
+    public Pair(int first, int second){
+        this.first = first;
+        this.second = second;
+    }
+    public int getFirst(){
+        return first;
+    }
+    public int getSecond(){
+        return second;
+    }
+    @Override
+    public int compareTo(Pair o){
+        return this.first - o.first;
+    }
+    @Override
+    public String toString(){
+        return "(" + first + ", " + second + ")";
+    }
+}
+
+//
 public class test {
     public static void main(String[] args) throws FileNotFoundException, IOException, ClassNotFoundException {
         // TODO code application logic here
