313eb1dedf13658eb9c65f35208c494a143c77dd
Nguyễn Hoàng Việt <108795111+Campus-is-me@users.noreply.github.com>
2022-12-20 02:04:22 +0700

    Add files via upload

---
+++ Tinh_Gio_Chuan.java
@@ -0,0 +1,41 @@
+//Created by Campus
+package tinh_gio_chuan;
+import java.lang.*;
+import java.math.*;
+import java.text.*;
+import java.util.*;
+import java.time.*;
+import java.io.*;
+//
+public class Tinh_Gio_Chuan {
+    public static void main(String[] args) throws FileNotFoundException {
+        // TODO code application logic here
+        Scanner mh = new Scanner(new File("MONHOC.in"));
+        Scanner gv = new Scanner(new File("GIANGVIEN.in"));
+        Scanner gc = new Scanner(new File("GIOCHUAN.in"));
+        int n = Integer.parseInt(mh.nextLine());
+        HashMap<String, String> subject = new HashMap<>();
+        for(int i = 0;i < n;i++){
+            String tmp = mh.nextLine();
+            subject.put(tmp.substring(0, 7), tmp.substring(8));
+        }
+        n = Integer.parseInt(gv.nextLine());
+        LinkedHashMap<String, GiangVien> teacher = new LinkedHashMap<>();
+        for(int i = 0;i < n;i++){
+            String tmp = gv.nextLine();
+            GiangVien otp = new GiangVien(tmp.substring(0, 4), tmp.substring(5));
+            teacher.put(otp.getCode(), otp);
+        }
+        int m = Integer.parseInt(gc.nextLine());
+        for(int i = 0;i < m;i++){
+            String tmp = gc.nextLine();
+            String code = tmp.substring(0, 4);
+            String codeSubject = tmp.substring(5, 12);
+            double point = Double.parseDouble(tmp.substring(13));
+            teacher.get(code).addTime(point);
+        }
+        for(Map.Entry<String, GiangVien> entry: teacher.entrySet()){
+            System.out.println(entry.getValue());
+        }
+    }
+}
